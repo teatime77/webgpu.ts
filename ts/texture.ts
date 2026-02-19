f41115fa722e5925dc1a17958ae10c46e2b6a3d2
teatime77 <teatime77@live.jp>
2026-02-19 22:49:24 +0900

    ../plane/images. pexels-james-wheeler-1552212.jpg

--- ts/texture.ts
+++ ts/texture.ts
@@ -143,7 +143,7 @@ export async function asyncBodyOnLoadTex() {
         {
             const img = document.createElement('img');
             img.crossOrigin = 'Anonymous';
-            img.src = 'lib/webgpu/pexels-james-wheeler-1552212.jpg';
+            img.src = '../plane/images/pexels-james-wheeler-1552212.jpg';
             await img.decode();
             const imageBitmap = await createImageBitmap(img);
 
